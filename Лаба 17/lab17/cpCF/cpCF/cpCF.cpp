422b16a4f4106b905255e2415ea466d2aae3a9a7
Cyberslayer30000 <91660180+Cyberslayer30000@users.noreply.github.com>
2021-11-02 23:37:34 +0300

    Add files via upload

---
+++ Лаба 17/lab17/cpCF/cpCF/cpCF.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+#include <locale.h>
+
+int main(int argc, LPTSTR argv[])
+{
+	setlocale(LC_ALL, "rus");
+	if (argc != 3)
+	{
+		printf("Использование: cpCF file1 file2\n");
+		return 1;
+	}
+	if (!CopyFile(argv[1], argv[2], FALSE))
+	{
+		printf("Ошибка CopyFile : %x\n", GetLastError());
+		return 2;
+	}
+	return 0;
+}
